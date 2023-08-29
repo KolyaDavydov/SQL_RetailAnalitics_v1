0039308d3dd508dfd57914c4b1849c25e4fe781f
nohoteth <nohoteth@student.21-school.ru>
2023-08-29 23:07:07 +0300

    fix part 3

--- src/part3.sql
+++ src/part3.sql
@@ -1,32 +1,33 @@
 SELECT rolname FROM pg_roles;
 
 -- Создаём групповые роли visitors_grp и admins_grp для облегчения управления
-
+-- TODO добавить права на таблицы
 CREATE OR REPLACE PROCEDURE proc_add_users_groups() 
 AS $$
 BEGIN
     CREATE ROLE visitors_grp NOLOGIN;
     GRANT SELECT ON ALL TABLES IN SCHEMA public TO visitors_grp;
-    GRANT USAGE ON SCHEMA public TO visitors_grp;
     CREATE ROLE admins_grp NOLOGIN;
-    GRANT SELECT ON ALL TABLES IN SCHEMA public TO admins_grp;
-    GRANT USAGE ON SCHEMA public TO admins_grp;
+    GRANT SELECT, UPDATE, INSERT, DELETE ON ALL TABLES IN SCHEMA public TO admins_grp;
+    -- GRANT USAGE ON SCHEMA public TO admins_grp;
 END;
 $$ LANGUAGE plpgsql;
 
 -- Процедура для создания пользователя и включения его в группу
-CREATE OR REPLACE PROCEDURE proc_add_user(user_name IN VARCHAR, user_password IN VARCHAR, user_role IN VARCHAR, res OUT INTEGER) 
+-- (имя пользователя, пароль, роль Administrators или Visitors, результат выполнения)
+
+CREATE OR REPLACE PROCEDURE proc_add_user(user_name IN VARCHAR, user_password IN VARCHAR, user_role IN VARCHAR, res OUT VARCHAR) 
 AS $$
 BEGIN
-    res = 0;
+    res = 'Failed';
     CASE WHEN user_role = 'Administrators' THEN
             EXECUTE 'CREATE ROLE ' || user_name || ' WITH LOGIN PASSWORD ''' || user_password || '''';
             EXECUTE 'GRANT admins_grp TO ' || user_name;
-        res = 1;
+        res = 'OK';
         WHEN user_role = 'Visitors' THEN
             EXECUTE 'CREATE ROLE ' || user_name || ' WITH LOGIN PASSWORD ''' || user_password || '''';
             EXECUTE 'GRANT visitors_grp TO ' || user_name;
-            res = 1;
+            res = 'OK';
         ELSE RETURN;
     END CASE;
     RETURN;
@@ -41,5 +42,4 @@ CALL proc_add_user('Administrator', 'super_pass111', 'Administrators',null);
 CALL proc_add_user('Visitor', '111_pass_super', 'Visitors',null);
 END;
 
-DROP ROLE visitors_grp;
-GRANT admins_grp ON  Administrator;
+DROP ROLE Visitor;
