c24e75cc16bf140376078031430e9352dfd27278
nohoteth <nohoteth@student.21-school.ru>
2023-09-03 20:36:33 +0300

    add groups by group_id in v_purchase_history

--- src/part2_2.sql
+++ src/part2_2.sql
@@ -1,14 +1,15 @@
--- Active: 1692707583285@@127.0.0.1@5421@sql3
+-- Active: 1692707583285@@127.0.0.1@5421@sql3_full
 CREATE VIEW v_purchase_history AS 
 SELECT Customer_ID, transactions.transaction_id, transaction_datetime, sku.Group_ID, 
-stores.SKU_Purchase_Price*checks.SKU_Amount AS Group_Cost, checks.sku_summ, checks.sku_summ_paid
+sum(stores.SKU_Purchase_Price*checks.SKU_Amount) AS Group_Cost, sum(checks.sku_summ) AS Group_Summ, sum(checks.sku_summ_paid) AS Group_Summ_Paid    
 FROM transactions
 JOIN cards ON cards.customer_card_id = transactions.customer_card_id
 JOIN checks ON checks.transaction_id = transactions.transaction_id
 JOIN sku ON sku.sku_id = checks.sku_id
 LEFT JOIN stores ON stores.transaction_store_id = transactions.transaction_store_id
 WHERE stores.sku_id = checks.sku_id
-ORDER BY Customer_ID, transactions.transaction_id, transaction_datetime;
+GROUP BY Customer_ID, transactions.transaction_id, sku.Group_ID
+ORDER BY Customer_ID, transactions.transaction_id, transaction_datetime, sku.Group_ID;
 
 SELECT * FROM v_purchase_history;
 
